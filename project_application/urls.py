aef737c286c74d76d20fb58c0068b0f887f623bb
kubapi <kuba.sliz@hotmail.com>
2020-03-30 17:37:27 +0200

    adding templates and connecting them with application view, setting up templates dir and urls

---
+++ project_application/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+#Used for template tagging
+app_name = 'project_application'
+
+urlpatterns = [
+    #name = '' is used for connecting with {% url %} in tamplates 
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact')
+]
--- project_application/views.py
+++ project_application/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main(request):
+    return render(request, 'project_application/main.html')
+
+def about(request):
+    return render(request, 'project_application/about.html')
+
+def contact(request):
+    return render(request, 'project_application/contact.html')
