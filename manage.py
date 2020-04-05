e0a81f232532336a83572108c9392dce3b01f357
kubapi <kuba.sliz@hotmail.com>
2020-04-06 00:18:47 +0200

    Changing direcotry to match with server one; also connecting databse from server and directory style!

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zawieszona_kawa.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'public_python.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
